78aa546a2c8102b6a55a07a43ac129bb4ba5f512
Jacob Halsey <jacob@jhalsey.com>
2021-07-31 01:56:42 +0100

    proof of concept calculator

--- include/ssc.h
+++ include/ssc.h
@@ -19,13 +19,14 @@ typedef struct
     double latitude;     // Observer latitude (negative south of equator)
     double longitude;    // Observer longitude (negative west of Greenwich)
 
-    // Defaults are available
-    double delta_ut1;    // Fractional second difference between UTC and UT which is used
-    double delta_t;      // Difference between earth rotation time and terrestrial time
-    double elevation;    // Observer elevation [meters]
-    double pressure;     // Annual average local pressure [millibars]
-    double temperature;  // Annual average local temperature [degrees Celsius]
-    double atmos_refract;// Atmospheric refraction at sunrise and sunset (0.5667 deg is typical)
+    // Defaults will be set when using `ssc_input_defaults`
+    double delta_ut1;      // Fractional second difference between UTC and UT which is used
+    double delta_t;        // Difference between earth rotation time and terrestrial time
+    double elevation;      // Observer elevation [meters]
+    double pressure;       // Annual average local pressure [millibars]
+    double temperature;    // Annual average local temperature [degrees Celsius]
+    double atmos_refract;  // Atmospheric refraction at sunrise and sunset (0.5667 deg is typical)
+    uint32_t step_size;    // Search step size
 } ssc_input;
 
 void ssc_input_defaults(ssc_input *input, uint64_t time, double latitude, double longitude);
--- src/ssc.c
+++ src/ssc.c
@@ -5,8 +5,9 @@
 //
 
 #include "ssc.h"
+#include "spa.h"
 
-void ssc_input_defaults(ssc_input *input, uint64_t time, double latitude, double longitude) {
+void ssc_input_defaults(ssc_input *input, unix_t time, double latitude, double longitude) {
     input->time = time;
     input->latitude = latitude;
     input->longitude = longitude;
@@ -16,5 +17,65 @@ void ssc_input_defaults(ssc_input *input, uint64_t time, double latitude, double
     input->pressure = 1013.25;
     input->temperature = 16.0;
     input->atmos_refract = 0.5667;
+    input->step_size = 3600;
 }
 
+static inline double jd_from_unix(unix_t t) {
+    return ( t / 86400.0 ) + 2440587.5;
+}
+
+// https://github.com/skyfielders/python-skyfield/blob/aa59e2d4711c3a95804170889f138402edbf4237/skyfield/almanac.py#L242
+static inline bool sun_is_up(spa_data *result) {
+    return result->e >= -0.8333;
+}
+
+#define ENSURE_SPA_RESULT   \
+    if (spa_result != 0) {  \
+        return spa_result;  \
+    }
+
+static int search_for_event(spa_data *data, unix_t start, int64_t step_size, bool target_upness, unix_t *result) {
+    int spa_result;
+    while (step_size != 0) {
+        data->jd = jd_from_unix(start);
+        spa_result = spa_calculate(data);
+        ENSURE_SPA_RESULT;
+        if (sun_is_up(data) == target_upness) {
+            step_size = -(step_size / 2);
+            target_upness = !target_upness;
+        } else {
+            start += step_size;
+        }
+    }
+    *result = start;
+    return 0;
+}
+
+int ssc(ssc_input *input, ssc_result *result) {
+    spa_data data;
+    int spa_result;
+
+    data.jd = jd_from_unix(input->time);
+    data.delta_ut1 = input->delta_ut1;
+    data.delta_t = input->delta_t;
+    data.longitude = input->longitude;
+    data.latitude = input->latitude;
+    data.elevation = input->elevation;
+    data.pressure = input->pressure;
+    data.temperature = input->temperature;
+    data.atmos_refract = input->atmos_refract;
+    spa_result = spa_calculate(&data);
+    if (spa_result) return spa_result;
+    result->visible = sun_is_up(&data);
+
+    unix_t *backward_time = result->visible ? &result->sunrise : &result->sunset;
+    unix_t *forward_time = result->visible ? &result->sunset : &result->sunrise;
+    int64_t step_signed = (int64_t) input->step_size;
+
+    spa_result = search_for_event(&data, input->time, -step_signed, !result->visible, backward_time);
+    ENSURE_SPA_RESULT;
+    spa_result = search_for_event(&data, input->time, step_signed, !result->visible, forward_time);
+    ENSURE_SPA_RESULT;
+
+    return 0;
+}
